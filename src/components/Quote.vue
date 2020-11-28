821913fdb4dad8629c3be680de42ed6afc9716ab
nemo D'acremont <nemo@nemo.dacremont@gmail.com>
2020-11-28 11:54:34 +0000

    Created Quote component

---
+++ src/components/Quote.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="quote">
+		<blockquote>
+			quote
+		</blockquote>
+		<cite>
+			from idk
+		</cite>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+
+});
+</script>
+
+<style lang="scss">
+
+</style>
