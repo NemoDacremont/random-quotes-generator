390afb14c94d4b95281b98d43c49134fa7b94112
nemo D'acremont <nemo@nemo.dacremont@gmail.com>
2020-11-28 13:28:54 +0000

    Added Quote component to Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="home">
+		<Quote />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
+import Quote from '@/components/Quote.vue';
+
 export default defineComponent({
-  name: 'Home',
+	name: 'Home',
+	components: {
+		Quote
+	}
 });
 </script>
